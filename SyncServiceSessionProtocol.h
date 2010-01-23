89f6611bbe0eccc5de5bceffb1c2a75c07524e70
Zachary Schneirov <scrod@scrodmbp.local>
2010-01-23 01:02:06 -0600

    description of a generic note sync service that for now closely resembles the capabilities of simplenote

---
+++ SyncServiceSessionProtocol.h
@@ -0,0 +1,63 @@
+//
+//  SyncServiceSession.h
+//  Notation
+//
+//  Created by Zachary Schneirov on 12/12/09.
+//  Copyright 2009 Zachary Schneirov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "SynchronizedNoteProtocol.h"
+
+@class SyncResponseFetcher;
+@class NotationPrefs;
+
+@protocol SyncServiceSession
+
+- (NSString*)localizedServiceTitle;
+- (NSString*)serviceName;
+- (NSString*)nameOfKeyElement;
+
+- (id)initWithNotationPrefs:(NotationPrefs*)prefs;
+
+- (BOOL)authorizationExpired;
+
+- (NSComparisonResult)localEntry:(NSDictionary*)localEntry isNewerThanRemoteEntry:(NSDictionary*)remoteEntry;
+- (BOOL)remoteEntryWasMarkedDeleted:(NSDictionary*)remoteEntry;
++ (void)registerModificationForNote:(id <SynchronizedNote>)aNote;
+
+- (void)stop;
+
+- (void)schedulePushForNote:(id <SynchronizedNote>)aNote;
+
+- (void)suppressPushingForNotes:(NSArray*)notes;
+- (void)stopSuppressingPushingForNotes:(NSArray*)notes;
+
+- (BOOL)startFetchingListForFullSync;
+
+- (void)startCollectingAddedNotesWithEntries:(NSArray*)entries mergingWithNotes:(NSArray*)someNotes;
+- (void)startCollectingChangedNotesWithEntries:(NSArray*)entries;
+
+- (void)startDeletingNotes:(NSArray*)notes;
+- (void)startModifyingNotes:(NSArray*)notes;
+- (void)startCreatingNotes:(NSArray*)notes;
+
+- (void)setDelegate:(id)aDelegate;
+- (id)delegate;
+
+@end
+
+@interface NSObject (SyncServiceSessionDelegate)
+
+//for showing the progress of various operations
+//- (void)syncResponseFetcherStarted:(SyncResponseFetcher*)fetcher forService:(id <SyncServiceSession>)syncSession;
+//- (void)syncResponseFetcherStopped:(SyncResponseFetcher*)fetcher forService:(id <SyncServiceSession>)syncSession;
+
+- (void)syncSession:(id <SyncServiceSession>)syncSession receivedFullNoteList:(NSArray*)allEntries;
+- (void)syncSession:(id <SyncServiceSession>)syncSession receivedAddedNotes:(NSArray*)addedNotes;
+- (void)syncSession:(id <SyncServiceSession>)syncSession didModifyNotes:(NSArray*)changedNotes;
+
+
+- (void)syncSessionDidFinishRemoteModifications:(id <SyncServiceSession>)syncSession;
+@end
