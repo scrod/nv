9cfb8ac6fb6bc7d018c3f34f333fb6a1aca84b31
Zachary Schneirov <scrod@scrodmbp.local>
2010-01-22 23:24:11 -0600

    base64 encoding/decoding based on openssl implementation

--- NSData_transformations.m
+++ NSData_transformations.m
@@ -24,6 +24,7 @@
 
 #include <unistd.h>
 #include <zlib.h>
+#include <openssl/bio.h>
 
 #import <WebKit/WebKit.h>
 
@@ -338,6 +339,38 @@
 	
 	return nil;
 }
+
+
+- (NSString *)encodeBase64 {
+    return [self encodeBase64WithNewlines:YES];
+}
+
+- (NSString *)encodeBase64WithNewlines:(BOOL)encodeWithNewlines {
+	
+    // Create a memory buffer which will contain the Base64 encoded string
+    BIO * mem = BIO_new(BIO_s_mem());
+    
+    // Push on a Base64 filter so that writing to the buffer encodes the data
+    BIO * b64 = BIO_new(BIO_f_base64());
+    if (!encodeWithNewlines)
+        BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+    mem = BIO_push(b64, mem);
+    
+    // Encode all the data
+    BIO_write(mem, [self bytes], [self length]);
+    (void)BIO_flush(mem);
+    
+    // Create a new string from the data in the memory buffer
+    char * base64Pointer;
+    long base64Length = BIO_get_mem_data(mem, &base64Pointer);
+    NSString * base64String = [[NSString alloc] initWithBytes:base64Pointer length:base64Length encoding:NSASCIIStringEncoding];
+    
+    // Clean up and go home
+    BIO_free_all(mem);
+    return [base64String autorelease];
+}
+
+
 
 
 @end
