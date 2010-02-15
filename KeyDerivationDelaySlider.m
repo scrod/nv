c0a429b5be15491115239b66f39a2d0cccd49d86
Zachary Schneirov <scrod@scrod.mmlc.northwestern.edu>
2010-02-15 15:29:21 -0600

    use a license header

--- KeyDerivationDelaySlider.m
+++ KeyDerivationDelaySlider.m
@@ -3,8 +3,17 @@
 //  Notation
 //
 //  Created by Zachary Schneirov on 11/11/06.
-//  Copyright 2006 Zachary Schneirov. All rights reserved.
-//
+
+/*Copyright (c) 2010, Zachary Schneirov. All rights reserved.
+  Redistribution and use in source and binary forms, with or without modification, are permitted 
+  provided that the following conditions are met:
+   - Redistributions of source code must retain the above copyright notice, this list of conditions 
+     and the following disclaimer.
+   - Redistributions in binary form must reproduce the above copyright notice, this list of 
+	 conditions and the following disclaimer in the documentation and/or other materials provided with
+     the distribution.
+   - Neither the name of Notational Velocity nor the names of its contributors may be used to endorse 
+     or promote products derived from this software without specific prior written permission. */
 
 #import "KeyDerivationDelaySlider.h"
 
