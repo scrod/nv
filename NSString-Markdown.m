74634f2ab662bf5718a71e1baf52ce32a52e6dff
Brett Terpstra <me@brettterpstra.com>
2010-12-05 19:55:50 -0600

    Added SmartyPants

--- NSString-Markdown.m
+++ NSString-Markdown.m
@@ -5,23 +5,36 @@
 + (NSString*)stringWithProcessedMarkdown:(NSString*)inputString
 {
 	NSString* mdScriptPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"MultiMarkdown.pl"];
+	NSString* spScriptPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"SmartyPants.pl"];
 	
 	NSTask* task = [[NSTask alloc] init];
-    NSMutableArray* args = [NSMutableArray array];
- 
+	NSMutableArray* args = [NSMutableArray array];
+	NSTask* task2 = [[NSTask alloc] init];
+	NSMutableArray* args2 = [NSMutableArray array];
+		
     [args addObject:mdScriptPath];
     [task setArguments:args];
+	[args2 addObject:spScriptPath];
+    [task2 setArguments:args2];
 	
 	NSPipe* stdinPipe = [NSPipe pipe];
 	NSPipe* stdoutPipe = [NSPipe pipe];
 	NSFileHandle* stdinFileHandle = [stdinPipe fileHandleForWriting];
 	NSFileHandle* stdoutFileHandle = [stdoutPipe fileHandleForReading];
-		
+	NSPipe* stdinPipe2 = [NSPipe pipe];
+	NSPipe* stdoutPipe2 = [NSPipe pipe];
+	NSFileHandle* stdinFileHandle2 = [stdinPipe2 fileHandleForWriting];
+	NSFileHandle* stdoutFileHandle2 = [stdoutPipe2 fileHandleForReading];
+	
 	[task setStandardInput:stdinPipe];
 	[task setStandardOutput:stdoutPipe];
+	[task2 setStandardInput:stdinPipe2];
+	[task2 setStandardOutput:stdoutPipe2];
 	
     [task setLaunchPath:@"/usr/bin/perl"];	
     [task launch];
+    [task2 setLaunchPath:@"/usr/bin/perl"];	
+    [task2 launch];
 	
 	[stdinFileHandle writeData:[inputString dataUsingEncoding:NSUTF8StringEncoding]];
 	[stdinFileHandle closeFile];
@@ -30,9 +43,18 @@
 	NSString* outputString = [[[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding] autorelease];
 	[stdoutFileHandle closeFile];
 
-	[task waitUntilExit];
+	[stdinFileHandle2 writeData:[outputString dataUsingEncoding:NSUTF8StringEncoding]];
+	[stdinFileHandle2 closeFile];
 
-	return outputString;
+	NSData* spOutputData = [stdoutFileHandle2 readDataToEndOfFile];
+	NSString* spOutputString = [[[NSString alloc] initWithData:spOutputData encoding:NSUTF8StringEncoding] autorelease];
+	[stdoutFileHandle2 closeFile];
+	
+	[task waitUntilExit];
+	[task2 waitUntilExit];
+	
+	
+	return spOutputString;
 }
 
 @end
