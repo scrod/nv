ec57ba75b9faeac1ab0f5f699b790e35f8eb9fb7
Zachary Schneirov <scrod@notational.net>
2011-03-17 13:10:07 -0500

    correct license for included smultron indentation code

--- LinkingEditor_Indentation.h
+++ LinkingEditor_Indentation.h
@@ -6,21 +6,27 @@
 //  Copyright 2006 Zachary Schneirov. All rights reserved.
 //
 
-/*Copyright (c) 2010, Zachary Schneirov. All rights reserved.
-    This file is part of Notational Velocity.
-
-    Notational Velocity is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Notational Velocity is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Notational Velocity.  If not, see <http://www.gnu.org/licenses/>. */
+/*
+ Modified code based on:
+ 
+ Smultron version 3.1.2, 2007-07-16
+ Written by Peter Borg, pgw3@mac.com
+ Find the latest version at http://smultron.sourceforge.net
+ 
+ Copyright 2004-2007 Peter Borg
+ 
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ use this file except in compliance with the License. You may obtain a copy
+ of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ License for the specific language governing permissions and limitations
+ under the License. 
+ */
 
 
 #import <Cocoa/Cocoa.h>
