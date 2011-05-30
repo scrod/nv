89320757c963d55a1b265d704e8900a1c9452f5f
Brett Terpstra <me@brettterpstra.com>
2011-05-30 12:57:10 -0500

    Adding recompiled AutoHyperlinks framework

---
+++ AutoHyperlinks.framework/Versions/A/Headers/AHLinkLexer.h
@@ -0,0 +1,45 @@
+/*
+ * The AutoHyperlinks Framework is the legal property of its developers (DEVELOPERS), whose names are listed in the
+ * copyright file included with this source distribution.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the AutoHyperlinks Framework nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ITS DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ITS DEVELOPERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#if __LP64__ || NS_BUILD_32_LIKE_64
+typedef long NSInteger;
+typedef unsigned long NSUInteger;
+#else
+typedef int NSInteger;
+typedef unsigned int NSUInteger;
+#endif
+
+typedef enum {
+    AH_URL_INVALID = -1,
+    AH_URL_VALID = 0,
+    AH_MAILTO_VALID,
+    AH_FILE_VALID,
+    AH_URL_DEGENERATE,
+    AH_MAILTO_DEGENERATE
+} AH_URI_VERIFICATION_STATUS;
+
+#define YY_EXTRA_TYPE NSUInteger
---
+++ AutoHyperlinks.framework/Versions/Current/Headers/AHMarkedHyperlink.h
@@ -0,0 +1,51 @@
+/*
+ * The AutoHyperlinks Framework is the legal property of its developers (DEVELOPERS), whose names are listed in the
+ * copyright file included with this source distribution.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the AutoHyperlinks Framework nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ITS DEVELOPERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ITS DEVELOPERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "AHLinkLexer.h"
+
+
+@interface AHMarkedHyperlink : NSObject <NSCopying> {
+    NSRange                      linkRange;
+    NSURL                       *linkURL;
+    NSString                    *pString;
+    AH_URI_VERIFICATION_STATUS      urlStatus;
+}
+
+-(id)initWithString:(NSString *)inString withValidationStatus:(AH_URI_VERIFICATION_STATUS)status parentString:(NSString *)pInString andRange:(NSRange)inRange;
+-(NSString *)parentString;
+-(NSRange)range;
+-(NSURL *)URL;
+-(AH_URI_VERIFICATION_STATUS)validationStatus;
+
+-(void)setRange:(NSRange)inRange;
+-(void)setURL:(NSURL *)inURL;
+-(void)setURLFromString:(NSString *)inString;
+-(void)setValidationStatus:(AH_URI_VERIFICATION_STATUS)status;
+-(void)setParentString:(NSString *)pInString;
+
+
+@end
