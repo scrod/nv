af01ef5418e623ed846a5c677ecfd265c65396a0
elasticthreads <elasticthreads@gmail.com>
2011-09-17 00:52:32 -0400

    Revert b6d8cc87bc7e925e1d92f036a4df937c11def620^..HEAD

--- AppController.m
+++ AppController.m
@@ -2611,9 +2611,11 @@ terminateApp:
 #endif
 
 - (BOOL)isInFullScreen{
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_7
     if (IsLionOrLater) {
        return (([window styleMask]&NSFullScreenWindowMask)>0);
     }
+#endif
     return [mainView isInFullScreenMode];
 
 }
