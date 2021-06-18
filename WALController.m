8742042b3d0da10d4a88c70040ebaeb67adf1541
Simon Grout <7671991-simongrout@users.noreply.gitlab.com>
2021-06-18 08:24:51 +0100

    Implement CFHashBytes CFHashBytes isn't accessible any more, but it's small and only called here

--- WALController.m
+++ WALController.m
@@ -32,6 +32,33 @@
 #import "NSCollection_utils.h"
 #import "NSString_NV.h"
 
+/*
+ CFHashBytes from http://www.opensource.apple.com/source/CF/CF-1153.18/CFUtilities.c
+ */
+#define ELF_STEP(B) T1 = (H << 4) + B; T2 = T1 & 0xF0000000; if (T2) T1 ^= (T2 >> 24); T1 &= (~T2); H = T1;
+
+CFHashCode CFHashBytes(uint8_t *bytes, CFIndex length) {
+    /* The ELF hash algorithm, used in the ELF object file format */
+    UInt32 H = 0, T1, T2;
+    SInt32 rem = (SInt32)length;
+    while (3 < rem) {
+        ELF_STEP(bytes[length - rem]);
+        ELF_STEP(bytes[length - rem + 1]);
+        ELF_STEP(bytes[length - rem + 2]);
+        ELF_STEP(bytes[length - rem + 3]);
+        rem -= 4;
+    }
+    switch (rem) {
+        case 3:  ELF_STEP(bytes[length - 3]);
+        case 2:  ELF_STEP(bytes[length - 2]);
+        case 1:  ELF_STEP(bytes[length - 1]);
+        case 0:  ;
+    }
+    return H;
+}
+
+#undef ELF_STEP
+
 //file descriptor based for lower level access
 
 //also used as an ad-hoc lock file;
