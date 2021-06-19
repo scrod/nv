29c0f846b58730f23632febc8040a4cbe9d56437
Simon Grout <7671991-simongrout@users.noreply.gitlab.com>
2021-06-19 08:18:41 +0100

    Declare volumeCapabilities before its called

--- FSExchangeObjectsCompat.h
+++ FSExchangeObjectsCompat.h
@@ -8,3 +8,4 @@
 
 OSErr FSExchangeObjectsEmulate(const FSRef *sourceRef, const FSRef *destRef, FSRef *newSourceRef, FSRef *newDestRef);
 Boolean VolumeOfFSRefSupportsExchangeObjects(const FSRef *fsRef);
+u_int32_t volumeCapabilities(const char *path);
